a1f338831b26099cd8f83964738a0ff8e2a4e3ef
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-09-02 10:36:44 -0700

    install HtmlWebpackPlugin and webpack-dev-server

---
+++ dist/main.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkjs_todo_list"] = self["webpackChunkjs_todo_list"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log('index js is connected')
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2pzLXRvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaW5kZXgganMgaXMgY29ubmVjdGVkJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To do List',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
