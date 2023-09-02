d8ee00d5260731eeae0bcbad0945984443cc571b
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-09-02 11:10:55 -0700

    add top nav

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -6,7 +6,24 @@
   \**********************/
 /***/ (() => {
 
-console.log('index js is connected')
+console.log('index js is connected');
+// nav bar
+const topNavContainer = document.createElement('div');
+topNavContainer.innerHTML = `
+  <div class="top-nav-container">
+    <div class="logo-container">
+      <img class="logo-image" src="logo.png" alt="TodoLite Logo">
+      <p class="logo-text">TodoLite</p>
+    </div>
+    <button class="add-task-button">Add Task</button>
+  </div>
+`;
+document.body.appendChild(topNavContainer);
+
+// side nav
+
+// task-list
+
 
 /***/ })
 
@@ -16,4 +33,4 @@ console.log('index js is connected')
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2pzLXRvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaW5kZXgganMgaXMgY29ubmVjdGVkJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL2pzLXRvZG8tbGlzdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaW5kZXgganMgaXMgY29ubmVjdGVkJyk7XG4vLyBuYXYgYmFyXG5jb25zdCB0b3BOYXZDb250YWluZXIgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbnRvcE5hdkNvbnRhaW5lci5pbm5lckhUTUwgPSBgXG4gIDxkaXYgY2xhc3M9XCJ0b3AtbmF2LWNvbnRhaW5lclwiPlxuICAgIDxkaXYgY2xhc3M9XCJsb2dvLWNvbnRhaW5lclwiPlxuICAgICAgPGltZyBjbGFzcz1cImxvZ28taW1hZ2VcIiBzcmM9XCJsb2dvLnBuZ1wiIGFsdD1cIlRvZG9MaXRlIExvZ29cIj5cbiAgICAgIDxwIGNsYXNzPVwibG9nby10ZXh0XCI+VG9kb0xpdGU8L3A+XG4gICAgPC9kaXY+XG4gICAgPGJ1dHRvbiBjbGFzcz1cImFkZC10YXNrLWJ1dHRvblwiPkFkZCBUYXNrPC9idXR0b24+XG4gIDwvZGl2PlxuYDtcbmRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQodG9wTmF2Q29udGFpbmVyKTtcblxuLy8gc2lkZSBuYXZcblxuLy8gdGFzay1saXN0XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('index js is connected')
+import './style.css';
+
+console.log('index js is connected');
+// nav bar
+const topNavContainer = document.createElement('div');
+topNavContainer.innerHTML = `
+  <div class="top-nav-container">
+    <div class="logo-container">
+      <img class="logo-image" src="logo.png" alt="TodoLite Logo">
+      <p class="logo-text">TodoLite</p>
+    </div>
+    <button class="add-task-button">+ Add Task</button>
+  </div>
+`;
+document.body.appendChild(topNavContainer);
+
+// side nav
+
+// task-list
